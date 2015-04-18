52c520137f0412318d7ad5549fc04bf0f6b189ae
Sergei Iakhnin <iakhnin@tbi-pbs2.inet.dkfz-heidelberg.de>
2015-04-18 23:10:41 +0200

    bug fixes

--- run_concordance.py
+++ run_concordance.py
@@ -1,6 +1,6 @@
 import os, fnmatch
 from subprocess import call
-import gzip, file, re
+import gzip, re
 
 rootdir = '/icgc/pcawg/analysis/iakhnin/germline_genotype_concordance'
 raw_dir = rootdir + '/raw/'
@@ -9,36 +9,37 @@ results_dir = rootdir + '/results/'
 header_file_name = "header.txt"
 snp6_vcf_dir = '/icgc/pcawg/analysis/waszak/tcga_snp6/vcf_files/'
 snp6_vcf_suffix = '.affy-snp6.cqc_0.4.qcr_0.86.birdseed-dev_2.6.cr_0.95.chr1_22.bi.snv.polymorphic.vcf.gz'
-#rtg_vcf_dir = "/icgc/pcawg/project/results/germline_variants/annai/tcga/"
-rtg_vcf_dir = "/icgc/pcawg/analysis/iakhnin/test/"
+rtg_vcf_dir = "/icgc/pcawg/project/results/germline_variants/annai/tcga/"
+#rtg_vcf_dir = "/icgc/pcawg/analysis/iakhnin/test/"
 snp6_to_rtg_mapping_file = "/icgc/pcawg/analysis/iakhnin/PCAWG_SNP6_uuid_to_annai_vcf_uuid.txt"
 header_size = 122
 
 def find_sample_files(pattern, path):
     print "Looking for RTG samples"
-    result = []
+    result = {}
     for root, dirs, files in os.walk(path):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 current_sample_filename = os.path.join(root, name)
-                result.append(current_sample_filename)
-    print len(result) + " samples found"
+                file_uuid = os.path.basename(root)
+                result[file_uuid] = current_sample_filename
+    print str(len(result)) + " samples found"
     return result
 
 def process_header(sample_filename):
     print "Processing header for " + sample_filename
     f = gzip.open(sample_filename, 'r')
-    h = file.open(raw_dir + header_file_name, 'w')
+    h = open(raw_dir + header_file_name, 'w')
     current_sample_uuid = ''
     for line in f:
         line_to_write = line
         if re.match('##reference=', line):
             line_to_write = '##reference=<ID=hs37d5>\n'
         if re.match('##SAMPLE=', line):
-            res = re.search('[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}\Z',line)
+            res = re.search('[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}',line)
             if res != None:
-                curret_sample_uuid = res.group(0)
-                line_to_write = '##SAMPLE=<ID=' + current_sample_uuid + '>'
+                current_sample_uuid = res.group(0)
+		line_to_write = '##SAMPLE=<ID=' + current_sample_uuid + '>'
         h.write(line_to_write)
         
         if re.match('#CHROM', line):
@@ -60,9 +61,9 @@ def regenerate_tabix(sample_filename):
 def get_snp6_to_rtg_mappings(mapping_file_path):
     snp6_to_rtg = {}
     rtg_to_snp6 = {}
-    f = file.open(mapping_file_path, "r")
-    for line in f.readline():
-        rec = line.split(",")
+    f = open(mapping_file_path, "r")
+    for line in f:
+        rec = line.strip().split(",")
         snp6_to_rtg[rec[0]] = rec[1]
         rtg_to_snp6[rec[1]] = rec[0]
     return snp6_to_rtg, rtg_to_snp6
@@ -70,12 +71,14 @@ def get_snp6_to_rtg_mappings(mapping_file_path):
 def get_results_file_name(snp6_uuid, rtg_uuid):
     return snp6_uuid + "_" + rtg_uuid + "_snp6_rtg_concordance_test.txt"
 
-def calculate_genotype_concordance(snp6_uuid, rtg_uuid, rtg_file_path):
+def calculate_genotype_concordance(rtg_uuid, snp6_uuid, rtg_file_path):
     print "Calculating genotype concordance between SNP6 sample - " + snp6_uuid + " and RTG sample - " + rtg_uuid 
     snp6_file_path = snp6_vcf_dir + snp6_uuid + snp6_vcf_suffix
     results_file_name = raw_dir + get_results_file_name(snp6_uuid, rtg_uuid) 
     os.chdir(raw_dir)
-    call("vcf-compare -a -g -m " + snp6_uuid + ":" + rtg_uuid + snp6_file_path + " " + rtg_file_path + "  > " + results_file_name, shell=True)
+    my_command = "vcf-compare -a -g -m " + snp6_uuid + ":" + rtg_uuid + " " + snp6_file_path + " " + rtg_file_path + "  > " + results_file_name
+    print "Running commad: " + my_command
+    call(my_command, shell=True)
     return results_file_name
 
 def clean_up(results_file_name):
@@ -89,12 +92,16 @@ def snp6_to_rtg_concordance_test():
     result_list = find_sample_files('*.vcf.gz', rtg_vcf_dir)
     snp6_to_rtg, rtg_to_snp6 = get_snp6_to_rtg_mappings(snp6_to_rtg_mapping_file)
     
-    for sample_file in result_list:
-        snp6_uuid = process_header(sample_file)
-        reheadered_sample_filename = reheader_sample(sample_file, snp6_uuid)
-        regenerate_tabix(reheadered_sample_filename)
-        results_file_name = calculate_genotype_concordance(snp6_uuid, snp6_to_rtg[snp6_uuid], reheadered_sample_filename)
-
+    for rtg_file_uuid in result_list.keys():
+        sample_file = result_list[rtg_file_uuid]
+        rtg_uuid = process_header(sample_file)
+        if rtg_to_snp6.has_key(rtg_file_uuid):
+            reheadered_sample_filename = reheader_sample(sample_file, rtg_uuid)
+            regenerate_tabix(reheadered_sample_filename)
+            results_file_name = calculate_genotype_concordance(rtg_uuid, rtg_to_snp6[rtg_file_uuid], reheadered_sample_filename)
+            clean_up(results_file_name)
+        else:
+            print "No corresponding SNP6 sample found for RTG sample " + rtg_file_uuid + ", skipping"
 snp6_to_rtg_concordance_test()    
     
     
