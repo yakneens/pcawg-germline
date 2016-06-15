a7d44ed08b821992632ce80c2a1b25a2caa20e30
Sergei Iakhnin <llevar@gmail.com>
2016-06-15 16:41:42 +0200

    Only create configs for samples that have missing files

--- scripts/prepare_missing_configs.py
+++ scripts/prepare_missing_configs.py
@@ -7,6 +7,7 @@ import datetime
 import uuid
 import json
 
+
 def write_config_to_file(config, config_location):
     
     run_uuid = str(uuid.uuid4())
@@ -20,7 +21,7 @@ tissue_type = "tumor"
 config_location = "/tmp/missing_configs"
 
 counter = 0
-f = open("missing_file_report", "w")
+
 contig_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                 "11", "12", "13", "14", "15", "16", "17", "18", "19", 
                 "20", "21", "22", "X", "Y"]
@@ -40,8 +41,15 @@ if tissue_type == "normal":
 else:
     sample_id = PCAWGSample.tumor_wgs_alignment_gnos_id
     sample_location = SampleLocation.tumor_sample_location 
+
+flag = True
     
 for root, dirs, files in os.walk(results_path):
+    
+    if flag:
+        flag = False
+        continue
+    
     missing_contigs = []
     this_sample_id = os.path.basename(root)
     for my_file in files:
@@ -50,19 +58,20 @@ for root, dirs, files in os.walk(results_path):
                 missing_contigs.append(my_contig)
     
 
-    
+    if len(missing_contigs) > 0:
 
-    current_sample = sample_session.query(PCAWGSample.index.label("index"), sample_id.label("sample_id"), sample_location.label("sample_location")).\
-        join(SampleLocation, PCAWGSample.index == SampleLocation.donor_index).\
-        filter(sample_id == this_sample_id).first()
-    
-    this_config_data = {"sample": {
-                                "donor_index": current_sample.index,
-                                "sample_id": current_sample.sample_id,
-                                "sample_location": current_sample.sample_location,
-                                "contig_names": missing_contigs
-                            }
-                        }
+        current_sample = sample_session.query(PCAWGSample.index.label("index"), sample_id.label("sample_id"), sample_location.label("sample_location")).\
+            join(SampleLocation, PCAWGSample.index == SampleLocation.donor_index).\
+            filter(sample_id == this_sample_id).first()
         
-    write_config_to_file(this_config_data, config_location)    
-f.close()
+        this_config_data = {"sample": {
+                                    "donor_index": current_sample.index,
+                                    "sample_id": current_sample.sample_id,
+                                    "sample_location": current_sample.sample_location,
+                                    "contig_names": missing_contigs
+                                }
+                            }
+            
+        write_config_to_file(this_config_data, config_location)
+        
+
