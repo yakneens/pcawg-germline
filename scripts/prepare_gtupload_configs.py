699770d5f22f174760f06c93c34d95041bce6129
Sergei Iakhnin <llevar@gmail.com>
2016-09-15 16:42:09 +0200

    First version of gtupload workflow.

---
+++ scripts/prepare_gtupload_configs.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+import os
+import uuid
+import json
+import argparse
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import or_, and_
+import re
+
+
+
+def parse_args():
+    my_parser = argparse.ArgumentParser()
+
+    sub_parsers = my_parser.add_subparsers()
+
+    create_configs_parser = sub_parsers.add_parser(
+        "create-configs", conflict_handler='resolve')
+    create_configs_parser.add_argument(
+        "-n", "--num_runs", help="Number of runs to create configurations for.", dest="num_runs", required=False, type=int)
+    create_configs_parser.add_argument(
+        "-c", "--config_location", help="Path to a directory where the generated config files will be stored.", dest="config_location", required=True)
+    create_configs_parser.add_argument(
+        "-s", "--sample_location", help="Path to a directory where the sample files are stored.", dest="sample_location", required=True)
+    
+    create_configs_parser.set_defaults(func=create_configs_command)
+
+    my_args = my_parser.parse_args()
+
+    return my_args
+
+
+def write_config_to_file(config, config_location):
+
+    run_uuid = str(uuid.uuid4())
+
+    my_file = open("{}/{}.json".format(config_location, run_uuid), "w")
+    json.dump(config, my_file)
+    my_file.close()
+
+
+def create_configs_command(args):
+
+    num_runs = args.num_runs
+    config_location = args.config_location
+    sample_location = args.sample_location
+    
+    num_configs = 0
+    
+    Base = automap_base()        
+    sample_engine = create_engine('postgresql://pcawg_admin:pcawg@postgresql.service.consul:5432/pcawg_sample_tracking')        
+    Base.prepare(sample_engine, reflect=True)        
+                
+    PCAWGSample = Base.classes.pcawg_samples
+                
+    sample_session = Session(sample_engine)
+    
+    sample_id = PCAWGSample.normal_wgs_aliquot_id
+    
+    
+            
+    
+    if (not os.path.isdir(config_location)):
+        os.makedirs(config_location)
+
+    
+    for root, dirs, files in os.walk(sample_location):
+        if num_runs == None or num_configs < num_runs:
+            sample_uuid = os.path.basename(root)
+            sample_data = sample_session.query(PCAWGSample.dcc_project_code.label("dcc_project_code"), PCAWGSample.submitter_donor_id.label("submitter_donor_id")).\
+                            filter(sample_id == sample_uuid).\
+                            first()
+            icgc_or_tcga = "ICGC"
+            if (re.search("_US$", sample_data.dcc_project_code):
+                icgc_or_tcga = "TCGA"
+                               
+                            
+            this_config_data = {
+                                "sample": {
+                                           "sample_id": sample_uuid,
+                                           "path_prefix": root,
+                                           "sample_files": files
+                                           },
+                                "dcc_project_code": sample_data.dcc_project_code,
+                                "submitter_donor_id": sample_data.submitter_donor_id,
+                                "icgc_or_tcga": icgc_or_tcga
+                                }
+            write_config_to_file(this_config_data, config_location)
+            num_configs = num_configs + 1
+        elif num_configs >= num_runs:
+            return
+
+
+    sample_session.close()
+    sample_engine.dispose()
+    
+
+if __name__ == '__main__':
+    args = parse_args()
+    args.func(args)
---
+++ workflows/gtupload-workflow/gtupload-workflow.py
@@ -0,0 +1,234 @@
+from airflow import DAG
+from airflow.operators import BashOperator, PythonOperator
+from datetime import datetime, timedelta
+
+import os
+import logging
+from subprocess import call
+
+from string import join
+
+import tracker.model
+from tracker.model.analysis_run import *
+from tracker.util.workflow_common import *
+
+import xml.etree.ElementTree as ET
+
+import uuid
+import os.path
+import datetime
+import re
+from xml.etree.ElementTree import SubElement
+from subprocess import check_output
+
+def prepare_sample_files_for_submission(submission_base_path, sample_id, sample_files):
+    if (not os.path.isdir(submission_base_path)):
+        logger.info(
+            "Submission directory {} not present, creating.".format(submission_base_path))
+        os.makedirs(submission_base_path)
+            
+    new_submission_id = str(uuid.uuid4())
+    os.makedirs("{}/{}".format(submission_base_path, new_submission_id))
+    
+    new_files = []
+    
+    for sample_file in sample_files:
+        ext = sample_file[sample_file.find("."):]
+        new_filename = "{}.butler-freebayes-1-0-0.{}.germline.snv{}".format(sample_id, datetime.datetime.now().strftime('%Y%m%d'), ext)
+        new_file_full_path = "{}/{}/{}".format(submission_base_path, new_submission_id)
+        copy_command = "cp {}/{} {}".format(sample_path_prefix,
+                                            sample_file,
+                                            new_filename)
+        
+        call_command(copy_command, "copy")
+        
+        md5 = check_output("md5sum " + new_file_full_path)
+        new_files.append((new_filename, md5))
+        
+    return new_files, new_submission_id
+
+def prepare_submission_metadata_from_template(metadata_template_location, sample_id, submission_full_path, dcc_project_code, submitter_donor_id, icgc_or_tcga):
+    analysis_template = ET.parse(metadata_template_location)
+
+    analysis_template.find("./ANALYSIS")[0].set("analysis_date", str(datetime.datetime.now()))
+    seq_labels = analysis_template.findall("./ANALYSIS/ANALYSIS_TYPE/REFERENCE_ALIGNMENT/SEQ_LABELS/SEQUENCE")
+    
+    for label in seq_labels:
+        label.set("data_block_name", sample_id)
+        
+    analysis_template.find("./ANALYSIS/TARGETS/TARGET").set("refname", sample_id)
+    
+    analysis_template.find("./ANALYSIS/DATA_BLOCK").set("name", sample_id)
+    
+    files_el = analysis_template.find("./ANALYSIS/DATA_BLOCK/FILES")
+    
+    for new_file in prepared_files:
+        file_el = SubElement(files_el, "FILE")
+        file_el.set("filename", new_file[0])
+        
+        el_type = ""
+        
+        if(re.search(".vcf.gz.tbi", new_file[0]) != None):
+            el_type = "idx"
+        else:
+            el_type = "vcf"
+        
+        file_el.set("filetype", el_type)
+        file_el.set("checksum_method", "MD5")
+        file_el.set("checksum", new_file[1])
+    
+    
+    
+    analysis_template.find("./ANALYSIS/ANALYSIS_ATTRIBUTES/ANALYSIS_ATTRIBUTE[TAG='dcc_project_code']/VALUE").text = dcc_project_code
+    analysis_template.find("./ANALYSIS/ANALYSIS_ATTRIBUTES/ANALYSIS_ATTRIBUTE[TAG='submitter_donor_id']/VALUE").text = submitter_donor_id
+    
+    analysis_template.write(submission_full_path + "/analysis.xml", "utf-8")      
+    
+
+def prepare_metadata(**kwargs):
+    config = get_config(kwargs)
+    sample = get_sample(kwargs)
+    
+    sample_id = sample["sample_id"]
+    sample_path_prefix = sample["path_prefix"]
+    sample_files = sample["sample_files"]
+    dcc_project_code = config["dcc_project_code"]
+    submitter_donor_id = config["submitter_donor_id"]
+    icgc_or_tcga = config["icgc_or_tcga"]
+    
+    metadata_template_location = config["metadata_template_location"]
+    submission_base_path = config["submission_base_path"]
+    
+    prepared_files, new_submission_id = prepare_sample_files_for_submission(submission_base_path, sample_id, sample_files)
+    
+    submission_sample_location =  submission_base_path + "/" + new_submission_id
+    
+    prepare_submission_metadata_from_template(metadata_template_location, sample_id, submission_sample_location, 
+                                              dcc_project_code, submitter_donor_id, icgc_or_tcga)
+    
+    return submission_sample_location
+
+def submit_metadata(**kwargs):
+    submission_sample_location = kwargs["ti"].xcom_pull(task_ids="prepare_metadata")
+    
+    config = get_config(kwargs)
+    sample = get_sample(kwargs)
+    icgc_or_tcga = config["icgc_or_tcga"]
+    
+    gnos = config["gnos"]
+    destination_repo_mapping = config["destination_repo_mapping"]
+    
+    selected_repo = gnos[destination_repo_mapping[icgc_or_tcga]]
+    
+    cgsubmit_command = "cgsubmit -u {} -s {} -c {}".format(submission_sample_location, selected_repo["url"], selected_repo["key_location"])
+    call_command(cgsubmit_command, "cgsubmit")
+
+def upload_sample(**kwargs):
+    submission_sample_location = kwargs["ti"].xcom_pull(task_ids="prepare_metadata")
+    
+    config = get_config(kwargs)
+    sample = get_sample(kwargs)
+    icgc_or_tcga = config["icgc_or_tcga"]
+    
+    gnos = config["gnos"]
+    destination_repo_mapping = config["destination_repo_mapping"]
+    
+    selected_repo = gnos[destination_repo_mapping[icgc_or_tcga]]
+    
+    gtupload_command = "gtupload {} -c {} -v".format(submission_sample_location + "/mainfest.xml", selected_repo["key_location"])
+    call_command(gtupload_command, "gtupload")
+    
+
+
+def bcftools(**kwargs):
+
+    config = get_config(kwargs)
+    sample = get_sample(kwargs)
+
+    sample_id = sample["sample_id"]
+    sample_path_prefix = sample["path_prefix"]
+    
+    filenames_list = sample["filename_list"]
+    full_path_filenames_list = map(lambda(x): sample_path_prefix + "/" + x, filenames_list)
+    filenames_string = " ".join(full_path_filenames_list)
+    
+    
+    result_path_prefix = config["results_local_path"] + "/" + sample_id
+    
+    bcftools_path = config["bcftools"]["path"]
+    bcftools_flags = config["bcftools"]["flags"]
+    bcftools_operation = config["bcftools"]["operation"]
+    
+    
+    if (not os.path.isdir(result_path_prefix)):
+        logger.info(
+            "Results directory {} not present, creating.".format(result_path_prefix))
+        os.makedirs(result_path_prefix)
+
+    result_filename = "{}/{}_merged.vcf.gz".format(
+        result_path_prefix, sample_id)
+
+    bcftools_command = 'bcftools {} {} {} -o {}'.\
+        format(bcftools_operation,
+               bcftools_flags,
+               filenames_string,
+               result_filename)
+    
+
+    call_command(bcftools_command, "bcftools")
+
+    generate_tabix(result_filename, config)
+    copy_result(result_filename, sample_id, config)
+   
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.datetime(2020, 01, 01),
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG("gtupload", default_args=default_args,
+          schedule_interval=None, concurrency=20000, max_active_runs=20000)
+
+
+start_analysis_run_task = PythonOperator(
+    task_id="start_analysis_run",
+    python_callable=start_analysis_run,
+    provide_context=True,
+    dag=dag)
+
+metadata_task = PythonOperator(
+    task_id="prepare_metadata",
+    python_callable=prepare_metadata,
+    provide_context=True,
+    dag=dag)
+
+metadata_task.set_upstream(start_analysis_run_task)
+
+cgsubmit_task = PythonOperator(
+    task_id="submit_metadata",
+    python_callable=submit_metadata,
+    provide_context=True,
+    dag=dag)
+
+cgsubmit_task.set_upstream(metadata_task)
+
+gtupload_task = PythonOperator(
+    task_id="upload_sample",
+    python_callable=upload_sample,
+    provide_context=True,
+    dag=dag)
+
+gtupload_task.set_upstream(cgsubmit_task)
+
+complete_analysis_run_task = PythonOperator(
+    task_id="complete_analysis_run",
+    python_callable=complete_analysis_run,
+    provide_context=True,
+    dag=dag)
+
+complete_analysis_run_task.set_upstream(gtupload_task)
