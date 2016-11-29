12a704f2e4bc020dfb8f0ecfa7e7ebf42b48eda1
Sergei Iakhnin <llevar@gmail.com>
2016-11-29 22:18:56 +0100

    Update script to use more intuitive parameter names and logging.

--- analyses/deletion-mutation-density/density_bins_by_project.R
+++ analyses/deletion-mutation-density/density_bins_by_project.R
@@ -4,31 +4,44 @@ library(VariantAnnotation)
 library(data.table)
 library(GenomicFeatures)
 library(progress)
+library(logging)
 
 'usage: density_bins_by_project.R [options]
 
 options:
- -p PROJECT_NAME  Project Name
- -m DONOR_META_PATH Donor  Meta Path
- -d DELETION_RANGES_PATH  Deletion Ranges Path
- -v SNV_RANGES_PATH SNV  Ranges Path
- -c CARRIER_MASK_PATH  Carrier Mask Path
- -r RESULT_PATH  Result Path' -> doc
+ --project_name PROJECT_NAME  Project Name
+ --non_carriers NON_CARRIERS  Non Carriers [default: FALSE]
+ --donor_meta_path DONOR_META_PATH Donor  Meta Path
+ --deletion_ranges_path DELETION_RANGES_PATH  Deletion Ranges Path
+ --snv_ranges_path SNV_RANGES_PATH SNV  Ranges Path
+ --carrier_mask_path CARRIER_MASK_PATH  Carrier Mask Path
+ --result_path RESULT_PATH  Result Path' -> doc
 library(docopt)
 
 opts = docopt(doc)
-project_name = opts$p
-donor_meta_path = opts$m
-deletion_ranges_path = opts$d
-snv_ranges_path = opts$v
-carrier_mask_path = opts$c
-result_path = opts$r
+project_name = opts$project_name
+donor_meta_path = opts$donor_meta_path
+deletion_ranges_path = opts$deletion_ranges_path
+snv_ranges_path = opts$snv_ranges_path
+carrier_mask_path = opts$carrier_mask_path
+result_path = opts$result_path
+non_carriers = as.logical(opts$non_carriers)
+
+if(non_carriers){
+  carrier_str = "non_carriers"
+}else{
+  carrier_str = "carriers"
+}
+
+loginfo("Parsed command-line options: %s", opts)
+
 
 load(donor_meta_path)
 load(deletion_ranges_path)
 load(snv_ranges_path)
 load(carrier_mask_path)
 
+loginfo("Loaded input data")
 
 get_binned_densities <- function(deletion_filter, snv_filter){
   snv_counts = unlist(lapply(snv_ranges, length))
@@ -43,6 +56,8 @@ get_binned_densities <- function(deletion_filter, snv_filter){
     filtered_deletion_ranges = deletion_ranges
     filtered_deletion_carrier_mask = deletion_carrier_mask
   }
+  loginfo("Filtered deletions. Retaining %d of total %d deletions.", length(filtered_deletion_ranges), length(deletion_ranges))
+  
   
   if(!is.null(snv_filter)){
     filtered_snv_ranges = snv_ranges[-snv_filter]
@@ -53,6 +68,8 @@ get_binned_densities <- function(deletion_filter, snv_filter){
     filtered_snv_counts = snv_counts
   }
   
+  loginfo("Filtered SNV samples. Retaining %d of total %d samples", length(filtered_snv_ranges), length(snv_ranges))
+  
   pre_deletion_ranges = trim(flank(filtered_deletion_ranges, width(filtered_deletion_ranges), start = T))
   post_deletion_ranges = trim(flank(filtered_deletion_ranges, width(filtered_deletion_ranges), start = F))
   
@@ -62,16 +79,27 @@ get_binned_densities <- function(deletion_filter, snv_filter){
   
   all_tiles = pc(pre_deletion_tiles, deletion_tiles, post_deletion_tiles)
   
-  hits = which(filtered_deletion_carrier_mask[,] > 0, arr.ind = T)
+  loginfo("Created %d density bins.", dim(all_tiles))
+  
+  if(non_carriers){
+    hits = which(filtered_deletion_carrier_mask == 0, arr.ind = T)
+  }else{
+    hits = which(filtered_deletion_carrier_mask > 0, arr.ind = T)
+  }
+  loginfo("Analyzing %s hits", dim(hits))
+  
   pb = progress_bar$new(format=":current/:total [:bar] :percent :elapsed :eta",total = dim(hits)[1])
   
   binned_densities = apply(hits, 1, function(x){pb$tick(); countOverlaps(all_tiles[[x[1]]], filtered_snv_ranges[[x[2]]]) / filtered_snv_counts[x[2]];})
+  
+  loginfo("Completed %s density bins.", dim(binned_densities))
   
   return(binned_densities)
   
 }
 
-var_name = paste("density_bins_", project_name, sep="")
+var_name = paste("density_bins_", project_name, "_", carrier_str, sep="")
 assign(var_name, get_binned_densities(NULL, which(donor_meta$dcc_project_code != project_name)))
-save(list=c(var_name), file=paste(result_path, "/", var_name, ".RData", sep=""))
-
+full_path = paste(result_path, "/", var_name, ".RData", sep="")
+save(list=c(var_name), file=full_path)
+loginfo("Saved densities to %s", full_path)
