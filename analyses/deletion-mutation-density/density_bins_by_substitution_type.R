c03326fb0759623bf14fc9757fa69feed33d48ca
Sergei Iakhnin <llevar@gmail.com>
2016-11-29 17:12:16 +0100

    Update data type for logging message.

--- analyses/deletion-mutation-density/density_bins_by_substitution_type.R
+++ analyses/deletion-mutation-density/density_bins_by_substitution_type.R
@@ -94,7 +94,7 @@ pre_deletion_tiles = tile(pre_deletion_ranges, n = 10)
 post_deletion_tiles = tile(post_deletion_ranges, n = 10)
 
 all_tiles = pc(pre_deletion_tiles, deletion_tiles, post_deletion_tiles)
-loginfo("Created %d density bins",  dim(all_tiles))
+loginfo("Created %s density bins",  dim(all_tiles))
 
 hits = which(filtered_deletion_carrier_mask[,] > 0, arr.ind = T)
 loginfo("Analyzing %s hits", dim(hits))
